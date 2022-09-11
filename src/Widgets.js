10faad80ec19f6b6a1e6e68a15aaf34f2cc3fe2e
Max Hoac <mhoac711@gmail.com>
2022-09-11 16:47:57 -0700

    Added widgets portion of application

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "./Widgets.css";
-
+import{
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed,
+} from 'react-twitter-embed';
+import SearchIcon from "@mui/icons-material/Search";
 
 function Widgets() {
     return (
         <div className="widgets">
-            <h2> Widgets</h2>
+            <div className="widgets_input">
+                <SearchIcon className="widgets_searchIcon" />
+                <input placeholder="Search Twitter" type="text" />
+            </div>
+            <div className="widgets_widgetContainer">
+                <h2>What's happening</h2>
+                <TwitterTweetEmbed tweetId={"1568706225939877888"} />
+                <h2>Who to follow</h2>
+                <TwitterTimelineEmbed
+                    sourceType = "profile"
+                    screenName = "DisguisedToast"
+                    options = {{height: 400}}
+                />
+                <TwitterShareButton
+                    url={"https://www.twitch.tv/disguisedtoast"}
+                    options = {{text: "bread is live", via: "DisguisedToast"}}
+                />
+            </div>
 
         </div>
     )
